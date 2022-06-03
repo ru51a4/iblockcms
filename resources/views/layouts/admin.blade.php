3b5218b99d4f4580e6edc0c35065c1a58a08bd6c
ru51a4 <ru51a4@mail.ru>
2022-06-03 12:42:42 +0500

    admini

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,199 +1,238 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.83.1">
-    <title>blogs</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/jumbotron/">
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
+    <!-- page meta -->
+    <meta name="description" content="Minimalistic Admin Panel built with Bootstrap 5"/>
+    <meta name="author" content="LeKoala"/>
+    <meta name="keywords" content="bootstrap, bootstrap 5, admin, panel, template, minimalistic"/>
+
+    <!-- performance -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+
+    <!-- page title & icon -->
+    <link
+        rel="icon"
+        type="image/svg+xml"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2252%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%22120%22>💠</text></svg>"
+    />
+    <title>Admini</title>
+
+    <!-- styles -->
+    <link href="/css/admini.min.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet"/>
+    <style>
+        .scroller {
+            padding: 10px;
+        }
+    </style>
+    <!-- icons -->
+    <script>
+        window.LastIcon = {
+            types: {
+                material: "twotone",
+            },
+            defaultSet: "material",
+            fonts: ["material"],
+        };
+    </script>
+    <script src="/js/last-icon.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Two+Tone" rel="stylesheet"/>
+
+    <!-- scripts -->
     <script
-        src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-        crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-            crossorigin="anonymous"></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <script src="/js/app.js"></script>
-    <style>
-        .container {
-            background: white;
-        }
-
-        .avatar {
-            margin: 10px;
-            max-width: 80px;
-            border: 2px solid black;
-        }
-
-        .nickname {
-            text-align: center
-        }
-
-        .nickname-author {
-            font-weight: bold;
-        }
-
-        .card:not(:last-child) {
-            border-bottom: 0px !important;
-        }
-
-        .card {
-            border-radius: 0px !important;
-            padding: 0px !important;
-        }
-
-        a:not(:last-child) .card {
-            border-bottom: 0px !important;
-        }
-
-        .card-avatar {
-            margin-top: 5px;
-        }
-
-        .dashboard a {
-            color: black;
-            text-decoration: auto;
-        }
-
-        .auth .nickname-author {
-            margin-right: 10px;
-        }
-
-        .container {
-            background-color: white !important;
-            margin: 20px;
-            border-radius: 10px;
-            min-height: 90vh;
-            display: flex;
-            flex-direction: column;
-
-        }
-
-        body {
-            background-color: #2d3748;
-        }
-
-        footer {
-            margin-top: 100%;
-            cursor: pointer;
-            justify-self: flex-end;
-        }
-
-        .card-body {
-            justify-content: space-between !important;
-            display: flex;
-            flex-direction: column;
-        }
-
-        .card-bottom {
-            display: flex;
-            justify-content: space-between;
-        }
-
-        .reply div {
-            color: black !important;
-        }
-
-        footer {
-            margin-top: auto !important;
-        }
-
-        .card-text {
-            word-break: break-all;
-        }
-
-        .card-avatar {
-            max-width: 10%;
-            min-width: 100px;
-        }
-
-        .status {
-            font-weight: 100;
-            font-size: 12px;
-            margin: 0px !important;
-        }
-
-        body {
-            display: flex;
-            justify-content: center;
-        }
-
-        .post_image {
-            max-width: 300px;
-        }
-
-        .reply {
-            cursor: pointer;
-            background-color: #f6993f;
-        }
-    </style>
-
-    <!-- Favicons -->
-    <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
-    <meta name="theme-color" content="#7952b3">
-
-
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
+        src="https://cdn.jsdelivr.net/gh/lekoala/nomodule-browser-warning.js/nomodule-browser-warning.min.js"
+        nomodule
+        defer
+        id="nomodule-browser-warning"
+    ></script>
+    <script type="module" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script type="module" src="/js/admini.min.js"></script>
+    <script type="module">
+        let sel = document.getElementById("sidebar-selector");
+        sel.addEventListener("change", (ev) => {
+            let color = sel.selectedOptions[0].style.backgroundColor;
+            if (color) {
+                document.querySelector(".sidebar-brand").style.backgroundColor = color;
+            } else {
+                document.querySelector(".sidebar-brand").style.backgroundColor = "inherit";
             }
-        }
-    </style>
-
-
+        });
+    </script>
+    <script type="module">window.admini.init(); // this should go last</script>
 </head>
-<body>
 
-<div class="container py-4">
-    <header class="pb-3 mb-4 border-bottom d-flex flex-row justify-content-between">
-        <div class="logo">
-            <a href="/admin" class="d-flex align-items-center text-dark text-decoration-none">
-                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2" viewBox="0 0 118 94"
-                     role="img"><title>Bootstrap</title>
-                    <path fill-rule="evenodd" clip-rule="evenodd"
-                          d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"
-                          fill="currentColor"></path>
-                </svg>
-                <span class="fs-4">IBLOCK CMS</span>
-        </div>
-        @if(auth()->user())
-            <div class="auth d-flex flex-row align-items-center">
-                <a href="/user">
-                    <div class="nickname-author">{{auth()->user() ? auth()->user()->name: ""}}</div>
-                </a>
-                <a href="/logout">
-                    <button type="submit" class="btn btn-primary">Выйти</button>
-                </a>
+<!-- minimenu class should be applied on render -->
+
+<body>
+<div class="wrapper">
+    <aside id="sidebar" class="sidebar" data-bs-scroll="true">
+        <a class="sidebar-brand" href="/admin">
+          <span class="sidebar-brand-icon">
+          </span>
+            <span class="sidebar-brand-title">IBLOCK CMS</span>
+        </a>
+
+        <!-- optional selector -->
+        <select name="" id="sidebar-selector" class="form-select">
+            <option value="">Main site</option>
+            <option value="" style="background: crimson; color: #fff">This site</option>
+            <option value="">That site</option>
+            <option value="">Other site with a long name&nbsp;&nbsp;</option>
+            <!-- cheap trick to add space after -->
+        </select>
+
+        <!-- optional dropdown or profile -->
+        <div class="sidebar-profile dropdown">
+            <button
+                class="btn btn-default"
+                id="profile-trigger"
+                data-bs-toggle="dropdown"
+                data-bs-display="static"
+                data-bs-offset="-32"
+                aria-expanded="false"
+            >
+                <img
+                    src="https://randomuser.me/api/portraits/thumb/men/75.jpg"
+                    class="avatar avatar-thumb rounded cursor-pointer"
+                    alt="John Nemo"
+                />
+                <div class="sidebar-profile-name dropdown-toggle">John Nemo</div>
+                <div class="sidebar-profile-subtitle">Administrator</div>
+            </button>
+
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="profile.html">Profile</a>
+                <a class="dropdown-item" href="#">Log out</a>
             </div>
-            @endif
-            </a>
-    </header>
-    @yield('content')
-    <footer class="pt-3 text-muted border-top">
-        superwebteam &copy; 2022
-    </footer>
+        </div>
+
+        <!-- sidebar-content -->
+        <div class="sidebar-content scroller">
+            <ul class="sidebar-nav">
+                <li class="sidebar-nav-header">UI</li>
+
+                <li class="sidebar-item active">
+                    <a class="sidebar-link" href="/admin">
+                        <span>Информационные блоки</span> </a>
+                </li>
+
+            <!-- this will collapse to a button when sidebar is collapsed -->
+
+        </div>
+        <!-- /sidebar-content -->
+
+        <!-- sidebar-footer -->
+        <div class="sidebar-footer">
+            <button class="btn btn-default sidebar-toggle js-sidebar-toggle">
+                <l-i name="menu_open"></l-i>
+            </button>
+            <div class="dropup">
+                <button class="btn btn-default" id="help-dropdown-btn" data-bs-toggle="dropdown">
+                    <l-i name="help_outline"></l-i>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="help-dropdown-btn">
+                    <li><a class="dropdown-item" href="help.html">Help</a></li>
+                    <li><a class="dropdown-item" href="faq.html">FAQ</a></li>
+                    <li>
+                        <hr class="dropdown-divider"/>
+                    </li>
+                    <p class="m-3 mb-0">
+                        <small>Built with Admini</small>
+                    </p>
+                </ul>
+            </div>
+        </div>
+        <!-- /sidebar-footer -->
+    </aside>
+
+    <main class="main">
+        <header class="main-header">
+            <div class="main-header-sidebar">
+                <button
+                    type="button"
+                    class="btn btn-primary btn-flex btn-square rounded-0"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#sidebar"
+                    aria-controls="sidebar"
+                >
+                    <l-i name="menu"></l-i>
+                </button>
+            </div>
+            <div class="main-header-info">
+                <nav aria-label="breadcrumb">
+                    <div class="breadcrumb">
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </div>
+                </nav>
+            </div>
+            <div class="main-header-nav">
+                <nav class="nav">
+                    <div class="nav-item">
+                        <div class="dropdown dropleft me-1">
+                            <button class="btn" id="notificationMenuLink" data-bs-toggle="dropdown"
+                                    data-bs-display="static" aria-expanded="false">
+                                <l-i name="notifications" size="24"></l-i>
+                                <span class="btn-indicator bg-danger">
+                      99+
+                      <span class="visually-hidden">unread messages</span>
+                    </span>
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg py-0"
+                                aria-labelledby="notificationMenuLink">
+                                <div class="dropdown-menu-header">
+                                    <h6>Notifications</h6>
+                                    <a href="">Mark as read</a>
+                                </div>
+                                <div class="list-group">
+                                    <a href="#" class="list-group-item list-group-item-action">
+                                        <div class="row g-0 align-items-center">
+                                            <div class="col-2">
+                                                <l-i name="info" class="text-danger"></l-i>
+                                            </div>
+                                            <div class="col-10">
+                                                <div class="d-flex w-100 justify-content-between">
+                                                    <h5 class="mb-1">Something bad happened</h5>
+                                                    <small>3 days ago</small>
+                                                </div>
+                                                <p class="mb-1">Some placeholder content in a paragraph.</p>
+                                                <small>And some small print.</small>
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action">
+                                        <div class="d-flex w-100 justify-content-between">
+                                            <h5 class="mb-1">List group item heading</h5>
+                                            <small class="text-muted">3 days ago</small>
+                                        </div>
+                                        <p class="mb-1">Some placeholder content in a paragraph.</p>
+                                        <small class="text-muted">And some muted small print.</small>
+                                    </a>
+                                    <a href="#" class="list-group-item list-group-item-action">
+                                        <div class="d-flex w-100 justify-content-between">
+                                            <h5 class="mb-1">List group item heading</h5>
+                                            <small class="text-muted">3 days ago</small>
+                                        </div>
+                                        <p class="mb-1">Some placeholder content in a paragraph.</p>
+                                        <small class="text-muted">And some muted small print.</small>
+                                    </a>
+                                </div>
+                                <a href="#" class="dropdown-menu-footer">See all notifications</a>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <section class="scroller">
+            @yield('content')
+        </section>
+    </main>
 </div>
 </body>
 </html>
