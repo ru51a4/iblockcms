597e28eafaf126029f9d6df98d3367120c119a5a
ru51a4 <ru51a4@mail.ru>
2023-01-17 22:38:32 +0500

    fix

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -7,35 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Service\Iblocks;
 use JWTAuth;
 
-/**
- * @OA\Get(
- * path="/api/index",
- *
- *  @OA\Parameter(
- *      name="id",
- *      in="query",
- *      required=true,
- *      @OA\Schema(
- *           type="number"
- *      )
- *   ),
- *   @OA\Parameter(
- *      name="page",
- *      in="query",
- *      required=true,
- *      @OA\Schema(
- *           type="number"
- *      )
- *   ),
- *   @OA\Response(
- *      response=200,
- *       description="catalog+els",
- *      @OA\MediaType(
- *           mediaType="application/json",
- *      )
- *   )
- *)
- */
 class IndexController extends Controller
 {
     public function __construct()
@@ -44,6 +15,35 @@ class IndexController extends Controller
     }
 
 
+    /**
+     * @OA\Get(
+     * path="/api/index",
+     *
+     *  @OA\Parameter(
+     *      name="id",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="number"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="page",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="number"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=200,
+     *       description="catalog+els",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   )
+     *)
+     */
     public function index($id = 1, $page = 1)
     {
         return Iblocks::GetList(1, $id, 5, $page, null, []);
