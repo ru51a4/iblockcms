26828859d06862e70127e72f75a70b9c25312187
ru51a4 <rruslanvaleev@yandex.ru>
2024-08-20 11:56:48 +0500

    fix

--- my-project/resources/views/layouts/app.blade.php
+++ my-project/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,7 +15,7 @@
     <!-- Bootstrap core CSS -->
     <!--Plugin CSS file with desired skin-->
     <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css"/>
+        href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css" />
 
     <!--jQuery-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -24,16 +25,13 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/js/ion.rangeSlider.min.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-            crossorigin="anonymous"></script>
-            <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <!-- Add the slick-theme.css if you want default styling -->
-    <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"
-    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
 
@@ -103,8 +101,6 @@
             display: flex;
             justify-content: center;
         }
-
-
     </style>
 
     <!-- Favicons -->
@@ -136,75 +132,85 @@
 
 
 </head>
+
 <body>
 
-<div class="container py-4">
-    <header class="pb-3 mb-4 border-bottom d-flex flex-row justify-content-start">
-        <div class="logo">
-            <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2" viewBox="0 0 118 94"
-                     role="img"><title>Bootstrap</title>
-                    <path fill-rule="evenodd" clip-rule="evenodd"
-                          d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"
-                          fill="currentColor"></path>
-                </svg>
-                <span class="fs-4">IBLOCK CMS</span>
-            </a>
+    <div class="container py-4">
+        <header class="pb-3 mb-4 border-bottom d-flex flex-row justify-content-start">
+            <div class="logo">
+                <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2"
+                        viewBox="0 0 118 94" role="img">
+                        <title>Bootstrap</title>
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"
+                            fill="currentColor"></path>
+                    </svg>
+                    <span class="fs-4">IBLOCK CMS</span>
+                </a>
 
-        </div>
-        <style>
-            .zhs {
-                position: absolute;
-                z-index: 100;
-                background: transparent;
-                display: none;
-            }
+            </div>
+            <style>
+                .zhs {
+                    position: absolute;
+                    z-index: 100;
+                    background: transparent;
+                    display: none;
+                }
 
-            .zhs-menu-event:hover .zhs {
-                display: block;
-            }
+                .zhs-menu-event:hover .zhs {
+                    display: block;
+                }
 
-            .zhs-menu {
-                display: flex;
-            }
+                .zhs-menu {
+                    display: flex;
+                }
 
-            .zhs-menu .zhs-menu--items {
-                display: inline-flex;
-                flex-direction: column;
-                background-color: transparent;
-            }
+                .zhs-menu .zhs-menu--items {
+                    display: inline-flex;
+                    flex-direction: column;
+                    background-color: transparent;
+                }
 
-            .zhs-menu .zhs-menu--items--item {
-                height: 50px;
-                margin: 1px;
-                transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out;
-                border-radius: .25rem;
-                padding: .5rem 1rem;
-                display: block;
-                cursor: pointer;
-                color: #fff;
-                background-color: #0d6efd;
-            }
-            .zhs-menu .zhs-menu--items--item a{
-                color: white;
-                text-decoration: none;
-            }
-        </style>
-        <ul class="d-xl-block d-none nav nav-pills">
-            <li class="nav-item zhs-menu-event"><a href="#" class="nav-link">Каталог</a>
-                <div class="zhs"></div>
-            </li>
+                .zhs-menu .zhs-menu--items--item {
+                    height: 50px;
+                    padding: .5rem 1rem;
+                    display: block;
+                    cursor: pointer;
+                    color: #fff;
+                    background-color: #0d6efd;
+                }
 
-        </ul>
-        @yield('zhsmenu')
+                .zhs-menu .zhs-menu--items--item:hover {
+                    height: 50px;
+                    padding: .5rem 1rem;
+                    display: block;
+                    cursor: pointer;
+                    color: #fff;
+                    background-color: purple;
+                }
 
-    </header>
-    @yield('content')
-    <footer class="pt-3 text-muted border-top">
-        This page took {{ (microtime(true) - LARAVEL_START) }} seconds to render
+                .zhs-menu .zhs-menu--items--item a {
+                    color: white;
+                    text-decoration: none;
+                }
+            </style>
+            <ul class="d-xl-block d-none nav nav-pills">
+                <li class="nav-item zhs-menu-event"><a href="#" class="nav-link">Каталог</a>
+                    <div class="zhs"></div>
+                </li>
 
-    </footer>
-</div>
+            </ul>
+            @yield('zhsmenu')
+
+        </header>
+        @yield('content')
+        <footer class="pt-3 text-muted border-top">
+            This page took {{ microtime(true) - LARAVEL_START }} seconds to render
+
+        </footer>
+    </div>
 
 </body>
+
 </html>
